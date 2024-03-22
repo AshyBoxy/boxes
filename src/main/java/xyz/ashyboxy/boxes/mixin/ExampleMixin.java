1b11a05afad6ac38ca5b2f9e95657c2a9f2fa68e
AshyBoxy <ashyboxy@ashyboxy.xyz>
2024-03-22 23:29:23 +0000

    wow it's the initial commit

---
+++ src/main/java/xyz/ashyboxy/boxes/mixin/ExampleMixin.java
@@ -0,0 +1,15 @@
+package xyz.ashyboxy.boxes.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "loadLevel")
+	private void init(CallbackInfo info) {
+		// here we run in the server's loadLevel method
+	}
+}
